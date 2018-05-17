e08090e833071f4edf8b07d49849c79e94931e84
adsrc <aditya16217@iiitd.ac.in>
2018-05-17 22:00:45 +0530

    Need to debug validation_tfidf.py

--- validation_tfidf.py
+++ validation_tfidf.py
@@ -11,6 +11,12 @@ from nltk.stem.snowball import SnowballStemmer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import KMeans
 from odds_ratio import get_odds_ratio
+import string
+import sys
+from gensim.models import Word2Vec
+from nltk import word_tokenize
+import operator
+
 
 stopwords = nltk.corpus.stopwords.words('english')
 
@@ -32,6 +38,8 @@ def purity_score(clusters, classes):
 
     :returns: the purity score
     :rtype: float
+
+    ref: http://www.caner.io/purity-in-python.html
     """
 
     A = np.c_[(clusters, classes)]
@@ -74,12 +82,17 @@ def tokenize_only(text):
             filtered_tokens.append(token)
     return filtered_tokens
 
+
 DIR_LINK_TRAIN = "data/convotev1.1/data_stage_one/training_set/"
 DIR_LINK_TEST = "data/convotev1.1/data_stage_one/test_set/"
-
+THRESHOLD = int(sys.argv[1])
+PUNCTUATION = string.punctuation
+print(THRESHOLD)
 training_speeches = os.listdir(DIR_LINK_TRAIN)
 
+
 tmp = []
+tmpn = []
 label_ref = {}
 labels = []
 z = 0
@@ -91,19 +104,32 @@ for speech in training_speeches:
             label_ref[speech[:3]] = z
             z += 1
         labels.append(label_ref[speech[:3]])
+    if speech[-5] == "N":
+        tmpn.append(speech)
 
+# Speeches from the training set labelled "Y"
 training_speeches = tmp
+training_speeches_n = tmpn
+
 
 fps = [open(DIR_LINK_TRAIN + file) for file in training_speeches]
+fps_n = [open(DIR_LINK_TRAIN + file) for file in training_speeches_n]
 
+# Content of the training speeches labelled "Y"
 train_content = [fp.read() for fp in fps]
+train_content_n = [fp.read() for fp in fps_n]
 
 for fp in fps:
 
     fp.close()
 
+for fp in fps_n:
+
+    fp.close()
+
 totalvocab_stemmed = []
 totalvocab_tokenized = []
+
 for i in train_content:
     # for each item in 'synopses', tokenize/stem
     allwords_stemmed = tokenize_and_stem(i)
@@ -113,19 +139,24 @@ for i in train_content:
     allwords_tokenized = tokenize_only(i)
     totalvocab_tokenized.extend(allwords_tokenized)
 
+
 vocab_frame = pd.DataFrame(
     {'words': totalvocab_tokenized}, index=totalvocab_stemmed)
 
-print(vocab_frame.head())
+
+# print(vocab_frame.head())
+
 
 tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
                                    min_df=0.2, stop_words='english',
                                    use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1, 3))
 
+
 tfidf_matrix = tfidf_vectorizer.fit_transform(
     train_content)  # fit the vectorizer to synopses
 
-print(tfidf_matrix.shape)
+
+# print(tfidf_matrix.shape)
 
 terms = tfidf_vectorizer.get_feature_names()
 
@@ -169,28 +200,10 @@ for i in range(true_k):
         print(' %s' % terms[ind], end='\n')
     print()
 
-
-# for i in range(num_clusters):
-#     print("Cluster %d words:" % i, end='')
-
-#     for ind in order_centroids[i, : 6]:  # replace 6 with n words per cluster
-#         print(' %s' % vocab_frame.ix[terms[ind].split(' ')].values.tolist()[
-#               0][0], end=',')
-#         central_words.append(vocab_frame.ix[terms[ind].split(' ')].values.tolist()[
-#             0][0])
-#     print()  # add whitespace
-#     print()  # add whitespace
-
-    # print("Cluster %d titles:" % i, end='')
-    # for title in frame.ix[i]['title'].values.tolist():
-    #     print(' %s,' % title, end='')
-    # print()  # add whitespace
-    # print()  # add whitespace
-
 print()
 print()
 
-print(central_words)
+# print(central_words)
 
 tracker = {}
 
@@ -215,24 +228,171 @@ for key in tracker:
 
     print(key, i)
 
-# highORwords, lowORwords = get_odds_ratio(os.listdir(DIR_LINK_TRAIN))
+highORwords, lowORwords = get_odds_ratio(os.listdir(DIR_LINK_TRAIN))
+
+yes_words = {}
+no_words = {}
+
+for speech in train_content:
+
+    speech = speech.strip(PUNCTUATION)
+    toks = speech.split(" ")
+    toks = [tok.strip() for tok in toks]
+    for i in range(len(toks)):
+        if toks[i] not in yes_words:
+            yes_words[toks[i]] = 1
+        else:
+            yes_words[toks[i]] += 1
+
+for speech in train_content_n:
+
+    speech = " ".join([word.strip(PUNCTUATION) for word in speech.split(" ")])
+    toks = speech.split(" ")
+    toks = [tok.strip() for tok in toks]
+    for i in range(len(toks)):
+        if toks[i] not in no_words:
+            no_words[toks[i]] = 1
+        else:
+            no_words[toks[i]] += 1
+
+above_threshold = []
+
+for key in yes_words.keys():
+    if yes_words[key] > THRESHOLD and key in no_words:
+        above_threshold.append((key, yes_words[key]))
+
+above_threshold = sorted(above_threshold, key=operator.itemgetter(1))
+all_speeches = train_content + train_content_n
+all_speeches_w2v = []
 
 
-# replacement = {}
-# print("$$$$$$$$$$$$$$$$$$$$$")
-# print(highORwords)
-# for word1 in highORwords:
+for speech in all_speeches:
 
-#     if word1 in central_words:
-#         print(word1)
+    speech = word_tokenize(speech)
+    all_speeches_w2v.append(speech)
 
-#     # max_sim = -1
+all_speeches.append(["unsound"])
 
-#     # for word2 in lowORwords:
 
-#     #     sim = model.similarity(word1, word2)
+model = Word2Vec(all_speeches_w2v)
 
-#     #     if sim > max_sim:
+mapping = {}
+i = 0
 
-#     #         max_sim = sim
-#     #         replacement[word1] = word2
+for tup in above_threshold:
+    word = tup[0]
+    max_sim = -1
+    if i < 100:
+        mapping[word] = ""
+        i += 1
+    else:
+        for wrd in lowORwords:
+            wrd = wrd.strip()
+            word = word.strip()
+            try:
+                if model.similarity(word, wrd) > max_sim:
+                    mapping[word] = wrd
+            except:
+                print(tup, wrd)
+
+final_speeches = []
+
+count_replaced = 0
+
+for speech in train_content:
+
+    tmp = speech.split(" ")
+
+    for i in range(len(tmp)):
+
+        if tmp[i] in mapping:
+            count_replaced += 1
+            # print(count_replaced)
+            tmp[i] = mapping[tmp[i]]
+
+    final_speeches.append(" ".join(tmp))
+
+
+# Now working on replaces speeches
+
+print("After replacement")
+
+
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
+                                   min_df=0.2, stop_words='english',
+                                   use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1, 3))
+
+
+tfidf_matrix = tfidf_vectorizer.fit_transform(
+    final_speeches)  # fit the vectorizer to synopses
+
+
+terms = tfidf_vectorizer.get_feature_names()
+
+# dist = 1 - cosine_similarity(tfidf_matrix)
+
+num_clusters = 38
+
+km = KMeans(n_clusters=num_clusters, random_state=10)
+
+km.fit(tfidf_matrix)
+
+
+clusters_aft = km.labels_.tolist()
+
+print("Purity score: ", purity_score(clusters_aft, labels))
+
+
+speeches = {'title': training_speeches,
+            'speech_content': final_speeches, 'cluster': clusters_aft}
+
+
+frame = pd.DataFrame(speeches, index=[clusters_aft], columns=[
+                     'title', 'cluster'])
+
+print(frame['cluster'].value_counts())
+
+
+print("Top terms per cluster:")
+print()
+# sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, :: -1]
+
+central_words = []
+
+true_k = np.unique(labels).shape[0]
+
+terms = tfidf_vectorizer.get_feature_names()
+for i in range(true_k):
+    print("Cluster %d:" % i, end='')
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind], end='\n')
+    print()
+
+print()
+print()
+
+# Tracker for the replaced speeches -------------
+
+tracker = {}
+
+for speech in final_speeches:
+
+    if speech[:3] not in tracker:
+        tracker[speech[:3]] = [0] * num_clusters
+
+for i in range(len(final_speeches)):
+
+    tracker[final_speeches[i][:3]][clusters_aft[i]] += 1
+
+for key in tracker:
+
+    mx = -1
+
+    for i in tracker[key]:
+
+        if i > mx:
+
+            mx = i
+
+    print(key, i)
